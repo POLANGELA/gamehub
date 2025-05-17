e40b2e869dc2851e1ec583ca4a660a5acff120aa
POLANGELA <ulissealessandromaria@gmail.com>
2025-05-17 17:38:52 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -27,6 +27,9 @@ def get_locale():
 def home_redirect():
     return redirect("/home")
 
+
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
