20c3d7d7c5f3b7ad105db7c5f322521040fbcb88
POLANGELA <ulissealessandromaria@gmail.com>
2025-05-17 15:13:19 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-# GameHub Business: supporta donazioni e pubblicità (ads)
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # RICHIESTO da Render
+    app.run(host="0.0.0.0", port=port)
